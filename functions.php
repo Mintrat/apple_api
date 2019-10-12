92efaad827e89e4585e6b7668e91b8c342374727
Viktor <ashvictor90@gmail.com>
2019-10-12 12:49:22 +0500

    add function getTop

--- functions.php
+++ functions.php
@@ -1,5 +1,12 @@
 <?php
 
+function getTop(String $path): ?array {
+    if (file_exists($path)) {
+        return json_decode(file_get_contents($path), true);
+    }
+}
+
+
 function show($data, $die = false) {
     echo '<pre>';
     print_r($data);
