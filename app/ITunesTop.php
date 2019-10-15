1f1ca7a183fac61ca2d1735807fe89ede3f38d90
Viktor <ashvictor90@gmail.com>
2019-10-15 13:21:38 +0500

    make Made the methods public.

--- app/ITunesTop.php
+++ app/ITunesTop.php
@@ -6,7 +6,7 @@ class ITunesTop
     private $http;
     private $urlSearch = 'https://api.music.apple.com/v1/catalog/us/songs/{id}';
     private $urlLookup = 'https://itunes.apple.com/lookup';
-    private $queryLookup = ['entity' => 'song', 'id' => ''];
+    private $queryLookup = ['entity' => 'song', 'amgArtistId' => ''];
 
     public function __construct(\GuzzleHttp\Client $http)
     {
@@ -15,35 +15,46 @@ class ITunesTop
 
     public function getTopArtistBySongId(int $songId, String $artistName): array
     {
-        $artistId = $this->getArtistIdBySongId($songId);
+        $artistsIds = $this->getArtistIdBySongId($songId);
 
-        if ($artistId !== false) {
+        if ($artistsIds !== false) {
+            $topArtists = [];
+
+            foreach ($artistsIds as $artistId) {
+                $topArtists[$artistId] = $this->getTopArtistById($artistId)                
+            }
+
             $topArtist = $this->getTopArtistById($artistId);
-            return $topArtist ? $topArtist : false;
+            return $topArtists;
         }
 
         return false;
     }
 
-    private function getArtistIdBySongId(int $idSong)
+    public function getArtistIdBySongId(int $idSong)
     {
         $query = str_replace('{id}', $idSong, $this->urlSearch);
         $response = $this->http->request('GET', $query);
         $jsonObj =  json_decode($response->getBody());
 
         if (!empty($jsonObj->data)) {
-            return $jsonObj->data[0]->relationships->artists->data[0]->id;
+            $artists = $jsonObj->data[0]->relationships->artists->data;
+            $idArtists = [];
+
+            for ($i = 0, $countAtrists = count($artists); $i < $countAtrists; ++$i) {
+                $idArtists[] = $artists[$i]->id;
+            }
+            return $idArtists;
         }
 
         return false;
     }
 
-    private function getTopArtistById(int $artistId)
+    public function getTopArtistById(int $artistId)
     {
         $queryLoop = $this->queryLookup;
-        $queryLoop['id'] = $artistId;
-        $query = $this->urlLookup . '?' . http_build_query($queryLoop);
-        $response = $this->http->request('GET', $query);
+        $queryLoop['amgArtistId'] = $artistId;
+        $response = $this->http->request('GET', $this->urlLookup, ['query' => $queryLoop]);
         $jsonObj = json_decode($response, true);
 
         if ($jsonObj) {
--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
+
 require './vendor/autoload.php';
-include './functions.php';
 
 use App\ITunesTop;
 use GuzzleHttp\Client;
 
-ini_set('max_execution_time', 0);
-
 $itunes = new ITunesTop(new GuzzleHttp\Client());
-foreach (getTop('./top.json')['results']['songs'][0]['data'] as $song) {
-    show($itunes->getTopArtistBySongId((int) $song['id']));
-}
+$topArtist = $itunes->getTopArtistBySongId(900032829);
